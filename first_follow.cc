60ad08be16c264ed5c4a5e90b8a5ce93a11ab111
Karthik Chandrasekar <karthidec1@gmail.com>
2013-09-22 20:21:19 -0700

    Modificaiton in the input receiving logic. Not fully functional though

--- first_follow.cc
+++ first_follow.cc
@@ -3,88 +3,85 @@
 
 using namespace std; 
 
-void getInput();
-void structureInput();
-void copyString(int, int, int, int);
+int getInput();
+void cleanInput(int);
+void copyToken(int, int, int, int);
 
 string grammar_array[100];
 string structured_input[100];
 
 int main () 
 {
-	getInput();
-	structureInput(); 
+	int count;
+	count = getInput();
+	cleanInput(count); 
 	return 0;
 } 
 
-void getInput()
+int getInput()
 {
 	string line;
-	int i=0;
-	cout << "Enter grammar speicification"<<endl;
+	int i, count =0;
+	cout << "Enter grammar specification"<<endl;
 	while(true)
 	{
 		getline(cin, line);
 		if (line.empty())
 			break;
-		grammar_array[i] = line;
-		i++;	
-					
+		grammar_array[count] = line;
+		count++;	
 	}
 
-	for(i=0;i< 100;i++)
+	for(i=0;i < count; i++)
 	{	
-		if (grammar_array[i].empty())
-			break;
 		cout << "Grammar specification" << "  " <<grammar_array[i]<<endl;
 	}
+	return count;
 }
 
-void structureInput()
+
+void cleanInput(int count)
 {
-	int i=0,j=0;
-	int start_i=0,end_i=0;
-	int start_j=0,end_j=0;
+	int rows_count = count;
+	int start_col = 0;	
+	int end_col = 0;
+	int rule_count = 0;
+	int i,j;		
 
-	for(i=0;;)
+	for(i=0;i<rows_count;i++)
 	{
-		if (grammar_array[i].empty())
-			break; 
-		for(j=0;;)
+		for(j=0;;j++)
 		{
-			if (grammar_array[i][j] == '#')
-			 	{
-					end_i = i;
-					end_j = j;
-					copyString(start_i, end_i, start_j, end_j);
-					start_i = i;
-					end_j = j;
-				}
-			i++;
-			j++;
-		}
-
-	}
-}
-
-void copyString(int start_i, int end_i, int start_j, int end_j)
-{
-	int count=0, total_char, i, j;
-	string new_sentence; 
-	total_char = end_i * end_j + start_i * start_j + 1;
-	for(i=start_i; ; i++)
-	{
-		for(j=start_j;;j++)
-		{
-			if(grammar_array[i][j] == '/0')
-				 break;
-			if(count <= total_char)
+			if(grammar_array[i][j] == '#')   //If we encounter # it is end of a rule
+			{
+				end_col = j;
+				copyToken(i,start_col, end_col, rule_count);
+				rule_count += 1;
+				if(grammar_array[i][j+1]=='#') //If there are two successive #'s it is end of grammar
 				{
-				count ++;
-				new_sentence[count] = grammar_array[i][j];
+					return;
 				}
-				
+				start_col = end_col;
+			}
+		
 		}
-
 	}
 }
+
+
+void copyToken(int row, int start_col, int end_col, int rule_count)
+{
+	int count =0;
+	int i;
+	
+	for(i=start_col;i<end_col;i++)
+	{
+		if (isspace(grammar_array[row][i]))
+		{
+			continue;
+		}
+		structured_input[rule_count][count] = grammar_array[row][i];
+		count ++;
+	}
+}
+
