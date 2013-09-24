e0efc9e88c8db14419b344175a2833e8e76bab01
Karthik Chandrasekar <karthidec1@gmail.com>
2013-09-24 16:27:35 -0700

    Added validating input functions for both terminals and non-terminals. These functions are not properly tested. Found a bug in input parsing which will be fixed in the check in.

--- first_follow.cc
+++ first_follow.cc
@@ -7,6 +7,9 @@ int getInput();
 void cleanInput(int);
 void copyToken(int, int, int, int);
 void printStructuredInput(int);
+int validateInput(int);
+int validateTerminals(int);
+int validateNonTerminals(int);
 
 string grammar_array[100];
 string structured_input[100];
@@ -14,9 +17,14 @@ string structured_input[100];
 int main () 
 {
 	int count;
+	int isValid;
+
 	count = getInput();
 	cleanInput(count);
 	printStructuredInput(count); 
+	isValid = validateInput(count);
+	if (isValid == 0)
+		cout << "Input is not valid"<<endl;
 	return 0;
 } 
 
@@ -53,7 +61,7 @@ void printStructuredInput(int count)
 		{
 			if (structured_input[i][j] == '\0')
 				break;
-			cout << "Structured Input " << structured_input[i][j]<<endl;
+			cout<<"i "<<i<<"  "<<"j "<<j<<" "<<"value "<<structured_input[i][j]<<" "<<endl;
 		}
 	}
 
@@ -105,5 +113,81 @@ void copyToken(int row, int start_col, int end_col, int rule_count)
 	
 }
 
+int validateInput(int count)
+{
+// Validate whether the entered input is proper
+	int isValidTerm;
+	int isValidNonTerm;
+
+   	isValidTerm = validateTerminals(count); 
+	isValidNonTerm = validateNonTerminals(count);
+	if (isValidTerm & isValidNonTerm)
+		{
+		return 1;
+		}
+	else
+		{
+		return 0;
+		}
+}
+
+int validateTerminals(int count)
+{
+// Check if All terminals have some rule associated with it
+
+
+	string terminals;
+	int term = 0;
+	int i, j, k;
+	
+	terminals = structured_input[0];
+
+	for(i=0;i<count;i++)
+	{
+		for(j=0,k=0;j<count;j++)
+		{
+			//cout<<"terminal "<<terminals[i]<<" "<<"Input "<<structured_input[j][k]<<" "<<"index "<<"i "<<i <<"j "<<j<<"k "<<k<<endl;
+			if (terminals[i] == structured_input[j][k])
+			   {		
+				term = 1;
+				break;
+			   }
+		}
+
+		if(term ==0 )
+			return 0;
+	}
+	return 1;
+}
+
+int validateNonTerminals(int count)
+{
+	string non_terminal;
+	int non_term = 0;
+	int i, j, k;
+
+	non_terminal = structured_input[1];
+	
+	for(i=0;i<count;i++)
+	{
+		for(j=0;j<count;j++)
+		{
+			for(k=0;;k++)
+			{
+				//cout<<"terminal "<<terminals[i]<<" "<<"Input "<<structured_input[j][k]<<" "<<"index "<<"i "<<i <<"j "<<j<<"k "<<k<<endl;
+				if (structured_input[j][k] == '\0')
+					break;
+				if (non_terminal[i] == structured_input[j][k])
+				{
+					non_term = 1;
+					break;
+				}
+			}
+		}
+		if (non_term ==0)
+			return 0;
+	}
+	return 1;
+}
 
 //Input getting part need to be revisited for sure. 
